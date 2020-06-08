57f81fa348294a4fac3e73e898b433eabde1e4b1
Kai Winter <kaiwinter@gmx.de>
2020-06-08 09:28:02 +0200

    async loading

--- src/main/java/com/github/kaiwinter/rclonediff/DiffController.java
+++ src/main/java/com/github/kaiwinter/rclonediff/DiffController.java
@@ -11,6 +11,7 @@ import com.github.kaiwinter.rclonediff.model.SyncFileStringConverter;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -74,6 +75,18 @@ public class DiffController implements Initializable {
 
   @FXML
   public void diff(ActionEvent event) {
+    Task<Void> task = new Task<>() {
+      @Override
+      public Void call() {
+        diff_internal();
+        return null;
+      }
+    };
+    task.setOnSucceeded(taskFinishEvent -> System.out.println(taskFinishEvent));
+    new Thread(task).start();
+  }
+
+  private void diff_internal() {
     RcloneWrapper main = new RcloneWrapper();
     try {
       main.check(localPath.getText(), remotePath.getText());
@@ -82,7 +95,6 @@ public class DiffController implements Initializable {
       remoteOnly.setItems(FXCollections.observableArrayList(main.getNotInLocal()));
 
     } catch (IOException | InterruptedException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     }
   }
